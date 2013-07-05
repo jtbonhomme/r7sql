e00491fe8fde478cd7f73c0c824a9d30c0b580ad
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2013-07-05 07:57:17 +0200

    Found request that work

--- sql/select_db_redmine.sql
+++ sql/select_db_redmine.sql
@@ -2,24 +2,19 @@ select
         db_remine.journals.journalized_id,
         db_remine.journals.created_on,
         db_remine.journal_details.old_value,
-        db_remine.journal_details.value 
+        db_remine.journal_details.value,
+        db_remine.issues.subject, 
+        db_remine.issues.created_on 
     from 
         db_remine.issues,
         db_remine.journals,
         db_remine.journal_details 
     where 
-        db_remine.journals.journalized_id in (
-            select 
-                db_remine.issues.id 
-            from 
-                db_remine.issues 
-            where 
-                db_remine.issues.tracker_id='1' 
-                and 
-                db_remine.issues.project_id='9' 
-                and db_remine.issues.status_id!='9'
-        )
+        db_remine.journals.journalized_id = db_remine.issues.id
         and
         db_remine.journals.id = db_remine.journal_details.journal_id
         and
-        db_remine.journal_details.prop_key = 'status_id' ;
+        db_remine.journal_details.prop_key = 'status_id' and db_remine.issues.tracker_id='1' 
+                and 
+                db_remine.issues.project_id='9' 
+                and db_remine.issues.status_id!='9';
